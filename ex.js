cca42077a84cced65cb14e36ce7430b0b80f164b
DergayPavel <53352812+DergayPavel@users.noreply.github.com>
2022-09-29 19:06:17 +0300

    percent upp3

--- ex.js
+++ ex.js
@@ -6,7 +6,7 @@ class Deposit{
     }
     saveDeposit(names, percent, capitalization){
         this.nameDeposit.push(names);
-        this.percent.push(percent/100/12);
+        this.percent.push(percent/100);
         this.capitalization.push(capitalization);    
     }
 }
@@ -19,7 +19,7 @@ function contributor(names, summ, month){
         if(deposits.nameDeposit[i]==names){
             let kolCap=Math.floor(month/deposits.capitalization[i]);
             for (let k=0; k<kolCap; k++){
-                summ+=summ*deposits.percent[i];
+                summ+=summ*deposits.percent[i]/12;
             }
         }
     }
